93300693658c3acd695074d0e068486144f3d859
Tim <kavumatimothy1@gmail.com>
2024-03-01 20:10:55 +0200

    Initial commit

---
+++ pihmalawi_config.py
@@ -0,0 +1,11 @@
+config = {
+   "endpoints" : [
+       {
+           "base": "",
+           "username": "",
+           "password": "",
+           "report_file": "teensclub_report.xlsx",
+           "config_file": "teensclub_config.csv",
+       }
+   ]
+}
